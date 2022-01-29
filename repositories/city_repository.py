85f9012b7f2a1dcf75e8054f351cb1d342eb81d4
Davebgla <91500089+Davebgla@users.noreply.github.com>
2022-01-29 16:09:52 +0000

    fourth commit

---
+++ repositories/city_repository.py
@@ -0,0 +1,41 @@
+from controllers import country
+from db.run_sql import run_sql
+
+from models.city import City
+from models.country import Country
+from models.user import User
+import repositories.country_repository as country_repository
+
+
+def save(city):
+    sql = "INSERT INTO cities (name, country_id) VALUES (%s, %s) RETURNING *"
+    values = [city.name, city.country.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    city.id = id
+    return city
+
+
+def select_all():
+    cities = []
+
+    sql = "SELECT * FROM cities"
+    results = run_sql(sql)
+
+    for row in results:
+        country = country_repository.select(row['country_id'])
+        city = City(row['name'], country, row['id'])
+        cities.append(city)
+    return cities
+
+
+def select(id):
+    city = None
+    sql = "SELECT * FROM cities WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = country_repository.select(result['country_id'])
+        city = City(result['name'], country, result['id'])
+    return city
