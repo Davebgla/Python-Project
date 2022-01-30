0fc7e44080555178ed9122b3d55904ff25b5a009
Davebgla <91500089+Davebgla@users.noreply.github.com>
2022-01-30 18:29:05 +0000

    controllers commit two

--- controllers/holidays_controller.py
+++ controllers/holidays_controller.py
@@ -1,7 +1,11 @@
 from flask import Flask, redirect, render_template, request
 from flask import Blueprint
-from repositories import holiday_repository, city_repository, country_repository, user_repository
 from models.holiday import Holiday
+import repositories.holiday_repository as holiday_repository
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+import repositories.user_repository as user_repository
+
 
 holidays_blueprint = Blueprint('holidays', __name__)
 
@@ -10,6 +14,33 @@ holidays_blueprint = Blueprint('holidays', __name__)
 def holidays():
     holidays = holiday_repository.select_all()
     return render_template('holidays/index.html', all_holidays = holidays)
+
+# NEW
+# GET '/holidays/new'
+@holidays_blueprint.route('/holidays/new', methods=['GET'])
+def new_holiday():
+    cities = city_repository.select_all()
+    countries = country_repository.select_all()
+    users = user_repository.select_all()
+    return render_template('holidays/new.html', cities = cities, countries = countries, users = users)
+
+# CREATE
+# POST '/holidays'
+@holidays_blueprint.route('/holidays', methods=['POST'])
+def create_holiday():
+    type = request.form['type']
+    country_id = request.form['country_id']
+    city_id = request.form['city_id']
+    review = request.form['review']
+    user_id = request.form['user_id']
+    country = country_repository.select(country_id)
+    city = city_repository.select(city_id)
+    user = user_repository.select(user_id)
+    holiday = Holiday(type, country, city, review, user)
+    holiday_repository.save(holiday)
+    return redirect('/holidays')
+
+
 
 
 
